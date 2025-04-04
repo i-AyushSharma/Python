edac22962c4b98b1d5a1c9820fd3e400beb9c84d
Ayush Sharma <ayushsharma2k8@gmail.com>
2025-04-04 20:04:16 +0530

    variables in python

--- basic/FirstProgram.py
+++ basic/FirstProgram.py
@@ -3,5 +3,5 @@ print("hello world")
 print("ayush")
 
 # hello, i am ayush
-print("hey i am a good boy \n and this is my code")
+print("hey i am a good boy \nand this is my code")
 print("my name is \"ayush\"")
---
+++ basic/Variable.py
@@ -0,0 +1,14 @@
+a = 64
+print(a)
+b = 7
+c = "Ayush"
+print(c)
+print(a * b)
+print("The type of variable a is ", type(a))
+print("The type of variable a is ", type(c))
+list1 = [8, 6, 5, 5.3, "apple", "banana"]
+print(list1)
+dict1 = {"name":"Ayush", "age":16, "can vote":False}
+print(dict1)
+list2 = ["apple", "banana"]
+print(list2)
