25b80300a595a65734d55e0cf3ec4d35a8c0a3df
Ayush Sharma <ayushsharma2k8@gmail.com>
2025-04-09 20:52:00 +0530

    Typecasting in Python

---
+++ basic/Typecasting.py
@@ -0,0 +1,12 @@
+string = "23"
+number = 7
+string_number = int(string) #throws an error if the string is not valid integer
+sum = number + string_number
+print("The sum of both the numbers is", sum)
+
+a = 8
+print(type(a))
+b = 9.7
+print(type(b))
+c = a + b
+print(type(c))
