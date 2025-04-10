af2950f7565d449d7d52769d29920118b5f4c00c
Ayush Sharma <ayushsharma2k8@gmail.com>
2025-04-10 12:41:36 +0530

    Strings

---
+++ basic/Strings.py
@@ -0,0 +1,18 @@
+apple = '''He said,
+"Rohan likes to eat apple
+but, Rohit don't"'''
+
+print(apple) #by putting triple, double or single code we can make anything a STRING
+
+name = "Ayush"
+print("hello," + name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4], "\n")
+#by doing this we can print the element of string correspnding to the number
+#Looping through the string
+print("lets use a for loop\n")
+for character in apple:
+    print(character)
