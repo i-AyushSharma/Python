55a9b4f4042437bf906e5f62d677131924a56b69
Ayush Sharma <ayushsharma2k8@gmail.com>
2025-05-29 11:31:40 +0530

    Changes

--- basic/IfElseStatement.py
+++ basic/IfElseStatement.py
@@ -25,15 +25,3 @@ elif(appleprice == budget):
 else:
     print("Sorry, You are out of  budget")
 print("Apple is my favourite fruit")
-
-# Exercise
-import time
-Timestamp = int(time.strftime("%H"))
-timestamp = time.strftime('%H:%M:%S')
-print(timestamp)
-if(4<=Timestamp<12):
-    print("Good Morning Sir, its", timestamp)
-elif(12>=Timestamp<17):
-    print("Good Afternoon Sir, its", timestamp)
-else:
-    print("Good Evening Sir, its", timestamp)
--- basic/Operators.py
+++ basic/Operators.py
@@ -5,12 +5,3 @@ print(15/6)
 print(15//6)
 print(5%3)
 print(15**2)
-
-#This is a Calculator
-
-a = 7
-b = 5
-print("The sum of", a, ("and"), b, ("is"), a+b)
-print("The product of", a, ("and"), b, ("is"), a*b)
-print("The difference of", a, ("and"), b, ("is"), a-b)
-print("The division of", a, ("and"), b, ("is"), a/b)
