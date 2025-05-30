4c7f11eff39c840496ede90763767250c445a59a
Ayush Sharma <ayushsharma2k8@gmail.com>
2025-05-30 12:29:28 +0530

    Doc Strings and PEP 8

---
+++ basic/DocStringsPEP8.py
@@ -0,0 +1,15 @@
+def square(n):
+    "Takes in a number n, returns the swuare of n"
+    print(n**2)
+square(5)
+print(square.__doc__)
+
+def square(n):
+    print(n)
+    "Takes in a number n, returns the swuare of n"
+    print(n**2)
+square(5)
+print(square.__doc__)
+
+import this #The Zen of Python
+
