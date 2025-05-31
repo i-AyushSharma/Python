de91231493aa82306c46c5a7a3deec4248b0534d
Ayush Sharma <ayushsharma2k8@gmail.com>
2025-05-31 19:21:43 +0530

    Doc Strings and PEP8(add on)

--- basic/DocStringsPEP8.py
+++ basic/DocStringsPEP8.py
@@ -13,3 +13,5 @@ print(square.__doc__)
 
 import this #The Zen of Python
 
+# PEP8(Python Enhancement Proposal)- PEP8 provides guidelines and practice of Python code. It was written by Guido van Rossum, Barry Warsaw and Nick Coghlan in 2001.
+# The  primary focus of PEP8 is to improve the readablity and consistency of python code. 
