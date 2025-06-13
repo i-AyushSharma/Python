704197a0f974fa689b77e0bb97ed81738acd9b8c
Ayush Sharma <ayushsharma2k8@gmail.com>
2025-06-13 20:33:58 +0530

    Enumerate Function(add on)

--- basic/EnumerateFunction.py
+++ basic/EnumerateFunction.py
@@ -1,12 +1,21 @@
 a = [12, 56, 32, 98, 12, 45, 1, 6]
 
+# index = 0
 # for mark in a:
 #     print(mark)
 #     if(index == 3):
 #         print("Harry")
 #     index += 1
 
-for index, mark in enumerate(a, start = 1):
+for index, mark in enumerate(a):
     print(mark)
     if(index == 3):
-        print("Harry")
+        print("Harry")  
+print("\n")
+
+# string
+
+string = ["apple", "banana", "grapes", "kiwi"]
+
+for index, fruit in enumerate(string):
+    print(index, fruit)
