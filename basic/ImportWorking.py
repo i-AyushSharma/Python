0b8de011a47b437e9dedbee39f1ac69853b36028
Ayush Sharma <ayushsharma2k8@gmail.com>
2025-06-15 20:40:59 +0530

    Working of Import

---
+++ basic/ImportWorking.py
@@ -0,0 +1,13 @@
+import math
+print(math.sqrt(9))
+
+from math import sqrt, pi
+print(math.sqrt(9)*pi)
+
+import math as m
+print(m.pi)
+
+print(dir(m))
+
+import Functions as fn
+print(fn.isGreater(65, 23))
