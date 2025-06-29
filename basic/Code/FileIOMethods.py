f3a2469f7e7b1c8cdc6f0237162c84a929b01237
Ayush Sharma <ayushsharma2k8@gmail.com>
2025-06-29 20:23:43 +0530

    Methods of FileIO-2

--- basic/Code/FileIOMethods.py
+++ basic/Code/FileIOMethods.py
@@ -1,4 +1,6 @@
-f = open('EgFile', 'r')
+#DAY 50
+
+f = open('EgFile.txt', 'r')
 while True:
     line = f.readline()
     if not line:
@@ -9,3 +11,18 @@ while True:
 # lines = ['line1\n, line2\netc.']
 # f.writelines(lines)
 # f.close()
+
+#DAY 51
+with open('EgFile.txt', 'r') as f:
+    print(type(f))
+    f.seek(10)
+    data = f.read(5)
+    print(data)
+    print(f.tell())
+
+# with open('EgFile.txt', 'r') as f:
+#     f.write('Hello World!')
+#     f.truncate(5)
+
+# with open('EgFile.txt', 'r') as f:
+#     print(f.read())
