ac4aa4ef1e35782266a9f12e0e44235399a4d033
Ayush Sharma <ayushsharma2k8@gmail.com>
2025-09-01 22:49:19 +0530

    'is' vs '=='

---
+++ basic/Code/'is'vs'=='.py
@@ -0,0 +1,17 @@
+a = 4 
+b = "4"
+
+print(a is b) # exact location of object in memory
+print(a == b) # value
+
+c = [3, 5, 2]
+d = [3, 5, 2]
+
+print(a is b)
+print(a == b)
+
+e = (6, 3, 2)
+f = (6, 3, 2)
+
+print(a is b)
+print(a == b)
